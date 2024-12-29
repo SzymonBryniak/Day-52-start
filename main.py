07440b1f3cd1583c8ae654e392df626582f529ec
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-12-29 20:05:30 +0000

    Day 52 progress

--- main.py
+++ main.py
@@ -54,14 +54,34 @@ class instagram_bot:
     time.sleep(3)
     followers = self.driver.find_element(By.XPATH, value="/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[3]/ul/li[2]/div/a").click()
     time.sleep(5)
-    button_list = self.driver.find_elements(By.CSS_SELECTOR, value="button")
+    self.driver.switch_to.active_element
+    button_list = self.driver.find_elements(By.TAG_NAME, value="button")
     print(len(button_list))
-    
-    for i in range(len(button_list)):
+
+    # button = self.driver.find_element(By.XPATH, value="/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]/div/button")
+    # # self.driver.execute_script("arguments[0].scrollIntoView();", button)
+    # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(button))
+    # button.click()
+    # time.sleep(3)
+    # cancel = self.driver.find_element(By.XPATH, value="/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div/button[2]").click()
+    for button in range(len(button_list)):
       time.sleep(5)
-      button = self.driver.find_element(By.XPATH, value="/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]/div/button")
-      
+      # button = self.driver.find_element(By.XPATH, value="/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]/div/button")
+      # button = self.driver.find_element(By.TAG_NAME, value="button")
+      button = self.driver.find_element(By.XPATH, value=f"/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[{button+1}]/div/div/div/div[3]/div/button")
+      # self.driver.execute_script("arguments[0].scrollIntoView();", button)
+      WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(button))
       button.click()
+      time.sleep(3)
+      self.driver.execute_script("arguments[0].scrollIntoView();", button)
+
+      try:
+        OK = self.driver.find_element(self.driver.find_element(By.XPATH, value="/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/button[2]"))
+      except:
+        print("OK not found")
+
+      time.sleep(3)
+      cancel = self.driver.find_element(By.XPATH, value="/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div/button[2]").click()
 
     # time.sleep(3)
     # for i in range(20):
@@ -69,7 +89,6 @@ class instagram_bot:
     #   test_click = self.driver.find_element(By.XPATH, value="/html/body/div[5]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]/div/div[1]/div/div/div/div[3]/div/button").click()
     #   time.sleep(2)
     #   cancel = self.driver.find_element(By.XPATH, value="/html/body/div [6]/div[1]/div/div[2]/div/div/div/div/div/div/button[2]").click()
-
 
 
 bot = instagram_bot()
